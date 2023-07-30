6f943116b9bc0b0efb0e7b6107fa1641366582c3
ljaraque <ljaraque@sabreman.home>
2023-07-30 19:08:22 -0400

    setting up environment variables

--- convergente/settings.py
+++ convergente/settings.py
@@ -11,6 +11,18 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 import os
 from pathlib import Path
+from dotenv import load_dotenv
+
+# Determine the environment from the DJANGO_ENV environment variable
+env = os.getenv('DJANGO_ENV', 'development')
+
+# Load the corresponding .env file based on the environment
+if env == 'development':
+    load_dotenv('.env.dev')
+elif env == 'staging':
+    load_dotenv('.env.staging')
+elif env == 'production':
+    load_dotenv('.env.prod')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,13 +32,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'ns((3u5uini1s81u649e8b9mx#4c9oht5-t*su=utjp7y5t!cy'
+#SECRET_KEY = 'ns((3u5uini1s81u649e8b9mx#4c9oht5-t*su=utjp7y5t!cy'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+#DEBUG = True
+DEBUG = int(os.environ.get("DEBUG", default=0))
 
-ALLOWED_HOSTS = ['192.168.1.92', '0.0.0.0', 'localhost']
-
+#ALLOWED_HOSTS = ['192.168.1.92', '0.0.0.0', 'localhost']
+ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
 
 # Application definition
 
@@ -75,7 +89,7 @@ WSGI_APPLICATION = 'convergente.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
@@ -86,6 +100,19 @@ DATABASES = {
         'PORT': '5432',
     }
 }
+'''
+
+DATABASES = {
+    "default": {
+        "ENGINE": os.environ.get("SQL_ENGINE", "django.db.backends.sqlite3"),
+        "NAME": os.environ.get("SQL_DATABASE", BASE_DIR / "db.sqlite3"),
+        "USER": os.environ.get("SQL_USER", "user"),
+        "PASSWORD": os.environ.get("SQL_PASSWORD", "password"),
+        "HOST": os.environ.get("SQL_HOST", "localhost"),
+        "PORT": os.environ.get("SQL_PORT", "5432"),
+    }
+}
+
 
 
 # Password validation
